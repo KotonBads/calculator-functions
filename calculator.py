1583d814e6584e68dc308bfcf768ff3e2c0e7df6
KotonBads <66117568+KotonBads@users.noreply.github.com>
2021-01-12 16:16:45 +0800

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+class add:
+    def __init__(self, num1, num2):
+        print(num1 + num2)
+class subtr:
+    def __init__(self, num1, num2):
+        print(num1 - num2)
+class multp:
+    def __init__(self, num1, num2):
+        print(num1 * num2)
+class divs:
+    def __init__(self, num1, num2):
+        print(num1 / num2)
+class pwr:
+    def __init__(self, num1, num2):
+        print(num1 ** num2)
